759546b83b292701d6525accc898a5418a10486b
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-04-17 23:46:01 +0800

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>DailyWins</h1>
+      <h4>are you ready to log your wins?</h4>
+      <label htmlFor="">Input1</label>
+      <input type="text"/>
+      <label htmlFor="">Input2</label>
+      <input type="text"/>
+      <label htmlFor="">Input3</label>
+      <input type="text"/>
+      <button>Log</button>
     </>
   )
 }
