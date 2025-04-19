e510a4a19cf60ca8a44eb747743e24f1ba996532
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-04-19 14:55:51 +0800

    setting up react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import './App.css';
 import Greet from './components/greet/Greet';
 import Input from './components/input/Input';
 import Button from './components/button/Button';
+import { Routes, Route, Link } from 'react-router-dom';
+import ViewLogs from './pages/ViewLogs';
 
 function App() {
   const [firstQ, setFirstQ] = useState('');
   const [secondQ, setSecondQ] = useState('');
   const [thirdQ, setThirdQ] = useState('');
 
-  const handleLog = () => {
+  function handleLog(e) {
+    e.preventDefault();
     console.log('One Thing I Learned:', firstQ);
     console.log('One Win:', secondQ);
     console.log('One Thing To Improve:', thirdQ);
@@ -20,27 +23,35 @@ function App() {
 
   return (
     <>
-      <p className="dailywins">DailyWins</p>
+      <div className='navbar'>
+        <p className="dailywins">DailyWins</p>
+        <Link to="/ViewLogs" className="view-logs">
+        <Button title="View Logs"/>
+        </Link>
+      </div>
+
       <Greet name="IRFAN" />
 
-      <div className="input-container">
-        <Input
-          title="One Thing I Learned"
-          value={firstQ}
-          onChange={(e) => setFirstQ(e.target.value)} // Update firstQ
-        />
-        <Input
-          title="One Win"
-          value={secondQ}
-          onChange={(e) => setSecondQ(e.target.value)} // Update secondQ
-        />
-        <Input
-          title="One Thing To Improve"
-          value={thirdQ}
-          onChange={(e) => setThirdQ(e.target.value)} // Update thirdQ
-        />
-        <Button title="Log" onClick={handleLog} />
-      </div>
+      <form onSubmit={handleLog}>
+        <div className="input-container">
+          <Input
+            title="One Thing I Learned"
+            value={firstQ}
+            onChange={(e) => setFirstQ(e.target.value)} // Update firstQ
+          />
+          <Input
+            title="One Win"
+            value={secondQ}
+            onChange={(e) => setSecondQ(e.target.value)} // Update secondQ
+          />
+          <Input
+            title="One Thing To Improve"
+            value={thirdQ}
+            onChange={(e) => setThirdQ(e.target.value)} // Update thirdQ
+          />
+          <Button title="Log" type="submit" />
+        </div>
+      </form>
     </>
   );
 }
---
+++ src/pages/ViewLogs.jsx
@@ -0,0 +1,8 @@
+export default function ViewLogs() {
+  return (
+    <div>
+      <h2>My Daily Logs</h2>
+      <p>This is the logs page where you'll see all your logged entries.</p>
+    </div>
+  )
+}
